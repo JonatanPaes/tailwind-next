5a7167e78edaf451a1b8d7ca689e4bb865e247f0
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2023-10-25 16:56:58 -0300

    feat: Estrutura do Layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,3 @@
 export default function Home() {
-  return (
-    <div className="flex h-screen flex-col items-center bg-slate-50 p-8 text-center text-slate-900 dark:bg-slate-900 dark:text-slate-100">
-      <div className="max-w-2xl">
-        <h1 className="flex items-center gap-3 text-3xl font-bold before:flex before:h-8 before:w-0.5 before:bg-sky-500 sm:text-4xl lg:text-5xl">
-          Why do we use it?
-        </h1>
-        <p className="mt-4 dark:text-slate-400">
-          It is a long established fact that a reader will be distracted by the
-          readable content of a page when looking at its layout. The point of
-          using Lorem Ipsum is that it has a more-or-less normal distribution of
-          letters, as opposed to using , making it look like readable English.
-          Many desktop publishing packages and web page editors now use Lorem
-          Ipsum as their default model text, and a search for will uncover many
-          web sites still in their infancy. Various versions have evolved over
-          the years, sometimes by accident, sometimes on purpose (injected
-          humour and the like).
-        </p>
-
-        <button className="mt-4 rounded-md bg-sky-500 px-2 py-2 font-medium text-white enabled:hover:bg-sky-600 disabled:cursor-not-allowed disabled:opacity-60 dark:bg-sky-400 dark:text-sky-950">
-          Sign in
-        </button>
-      </div>
-    </div>
-  )
+  return <h1>Heloo World</h1>
 }
