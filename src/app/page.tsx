5b0be6e16b92d943c2b71859f6956fde0eb735f1
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2023-10-25 10:35:29 -0300

    feat: dark mode no tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,26 @@
 export default function Home() {
   return (
-    <div className="p-8 bg-slate-500 text-slate-100 h-screen">
-      <h1 className="font-bold text-3xl sm:text-5xl lg:text-6xl flex items-center gap-3 before:w-0.5 before:h-8 before:bg-sky-500 before:flex">
-        Hello Tailwind
-      </h1>
-      <h2>Hello Tailwind</h2>
-      <p>Hello Tailwind</p>
+    <div className="p-8 bg-slate-50 text-slate-900 dark:bg-slate-900 dark:text-slate-100 h-screen flex flex-col items-center text-center">
+      <div className="max-w-2xl">
+        <h1 className="font-bold text-3xl sm:text-4xl lg:text-5xl flex items-center gap-3 before:w-0.5 before:h-8 before:bg-sky-500 before:flex">
+          Why do we use it?
+        </h1>
+        <p className="mt-4 dark:text-slate-400">
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsum is that it has a more-or-less normal distribution of
+          letters, as opposed to using 'Content here, content here', making it
+          look like readable English. Many desktop publishing packages and web
+          page editors now use Lorem Ipsum as their default model text, and a
+          search for 'lorem ipsum' will uncover many web sites still in their
+          infancy. Various versions have evolved over the years, sometimes by
+          accident, sometimes on purpose (injected humour and the like).
+        </p>
 
-      <button
-        disabled
-        className="bg-sky-500 px-2 py-2 rounded-md font-medium mt-4 enabled:hover:bg-sky-600 disabled:opacity-60 disabled:cursor-not-allowed"
-      >
-        Sign in
-      </button>
+        <button className="bg-sky-500 dark:bg-sky-400 dark:text-sky-950 text-white px-2 py-2 rounded-md font-medium mt-4 enabled:hover:bg-sky-600 disabled:opacity-60 disabled:cursor-not-allowed">
+          Sign in
+        </button>
+      </div>
     </div>
   );
 }
