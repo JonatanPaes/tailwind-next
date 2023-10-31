3c8d1933a62c6e48f1672976c2780c201192ac3f
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2023-10-30 21:30:43 -0300

    feat: Listagem de Arquivos

--- src/components/Form/FileInput/Root.tsx
+++ src/components/Form/FileInput/Root.tsx
@@ -12,7 +12,7 @@ export type RootProps = ComponentProps<'div'>
 type FileInputContextType = {
   id: string
   files: File[]
-  onFilesSelected: (files: File[]) => void
+  onFilesSelected: (files: File[], multiple: boolean) => void
 }
 
 const FileInputContex = createContext({} as FileInputContextType)
@@ -21,8 +21,16 @@ export function Root(props: RootProps) {
   const id = useId()
   const [files, setFiles] = useState<File[]>([])
 
+  function onFilesSelected(files: File[], multiple: boolean) {
+    if (multiple) {
+      setFiles((state) => [...state, ...files])
+    } else {
+      setFiles(files)
+    }
+  }
+
   return (
-    <FileInputContex.Provider value={{ id, files, onFilesSelected: setFiles }}>
+    <FileInputContex.Provider value={{ id, files, onFilesSelected }}>
       <div {...props} />
     </FileInputContex.Provider>
   )
