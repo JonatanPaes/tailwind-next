c760fbd74e891bc335cb4e3d725821a4c398c969
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2023-10-31 00:15:58 -0300

    feat: Criando Select de Pais

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import { InputControl, InputPrefix, InputRoot } from '@/components/Input'
-import { SettingsTabs } from '@/components/SettingsTabs'
-import { Mail } from 'lucide-react'
 import * as FileInput from '@/components/Form/FileInput'
+
+import { SettingsTabs } from '@/components/SettingsTabs'
+import { Select } from '@/components/Form/Select'
+
+import { Mail } from 'lucide-react'
 
 export default function Home() {
   return (
@@ -113,7 +116,7 @@ export default function Home() {
             >
               Country
             </label>
-            <div></div>
+            <Select />
           </div>
 
           <div className="grid grid-cols-form gap-3 pt-5">
